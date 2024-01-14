d002ba5e9ff1662499eacbd32643c78546d0cac1
seastamp <seastamp10@gmail.com>
2024-01-14 17:46:32 +0900

    Feat: ESLint에 걸리는 코드 삭제

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,12 +1,7 @@
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./page/Login.jsx";
 import Signup from "./page/Signup.jsx";
-import React from "react";
 import Main from "./page/Main.jsx";
-
-function AuthLayout() {
-    return null;
-}
 
 const Router = () => {
     return (
--- src/page/Login.jsx
+++ src/page/Login.jsx
@@ -1,4 +1,3 @@
-import * as React from "react";
 import {useState} from "react";
 import Container from "@mui/material/Container";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -16,104 +15,101 @@ import {useNavigate} from "react-router-dom";
 import {setAccessToken, setRefreshToken} from "../api/common/cookie.js";
 
 const Login = () => {
-  const navigate = useNavigate();
-  const locationSignup = () => {
-    navigate('/signup');
-  }
-  const locationMain = () => {
-    navigate('/');
-  }
-  const [user, setUser] = useState({
-    email: '',
-    password: '',
-  });
-
-  const handleInputChange = (event) => {
-    const {name, value} = event.target;
-    setUser((prevUser) => ({
-      ...prevUser,
-      [name]: value,
-    }));
-  };
-
-  const handleLogin = async (event) => {
-    event.preventDefault();
-    await login(user).then((response) => {
-      alert("로그인에 성공하였습니다!");
-      const accessToken = response.headers.get("AccessToken")
-      const refreshToken = response.headers.get("RefreshToken");
-      setAccessToken(accessToken);
-      setRefreshToken(refreshToken);
-      navigate("/");
-    }).catch((error) => {
-     console.log(error);
+    const navigate = useNavigate();
+    const locationSignup = () => {
+        navigate('/signup');
+    }
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
     });
-  };
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline/>
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{m: 2, bgcolor: '#9ccc65'}}>
-          <LoginIcon/>
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" sx={{mt: 3}}>
-          <Grid container spacing={2}>
-            <Grid item xs={12} >
-              <TextField
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                value={user.email}
-                autoComplete="email"
-                onChange={handleInputChange}
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                required
-                fullWidth
-                id="password"
-                label="Password"
-                name="password"
-                value={user.password}
-                type="password"
-                autoComplete="password"
-                onChange={handleInputChange}
-              />
-            </Grid>
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            onClick={handleLogin}
-            variant="contained"
-            sx={{mt: 3, mb: 2, bgcolor: '#3f9df9'}}
-          >
-            Login
-          </Button>
-          <Grid container justifyContent="flex-end">
-            <Link onClick={locationSignup}variant="body2">
-              가입하신 적이 없으시나요? 회원가입
-            </Link>
-          </Grid>
-        </Box>
-      </Box>
-      <Footer/>
-    </Container>
-  );
+    const handleInputChange = (event) => {
+        const {name, value} = event.target;
+        setUser((prevUser) => ({
+            ...prevUser,
+            [name]: value,
+        }));
+    };
+
+    const handleLogin = async (event) => {
+        event.preventDefault();
+        await login(user).then((response) => {
+            alert("로그인에 성공하였습니다!");
+            const accessToken = response.headers.get("AccessToken")
+            const refreshToken = response.headers.get("RefreshToken");
+            setAccessToken(accessToken);
+            setRefreshToken(refreshToken);
+            navigate("/");
+        }).catch((error) => {
+            console.log(error);
+        });
+    };
+
+    return (
+        <Container component="main" maxWidth="xs">
+            <CssBaseline/>
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+                <Avatar sx={{m: 2, bgcolor: '#9ccc65'}}>
+                    <LoginIcon/>
+                </Avatar>
+                <Typography component="h1" variant="h5">
+                    Login
+                </Typography>
+                <Box component="form" sx={{mt: 3}}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                value={user.email}
+                                autoComplete="email"
+                                onChange={handleInputChange}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                required
+                                fullWidth
+                                id="password"
+                                label="Password"
+                                name="password"
+                                value={user.password}
+                                type="password"
+                                autoComplete="password"
+                                onChange={handleInputChange}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                        type="submit"
+                        fullWidth
+                        onClick={handleLogin}
+                        variant="contained"
+                        sx={{mt: 3, mb: 2, bgcolor: '#3f9df9'}}
+                    >
+                        Login
+                    </Button>
+                    <Grid container justifyContent="flex-end">
+                        <Link onClick={locationSignup} variant="body2">
+                            가입하신 적이 없으시나요? 회원가입
+                        </Link>
+                    </Grid>
+                </Box>
+            </Box>
+            <Footer/>
+        </Container>
+    );
 }
 
 export default Login;
