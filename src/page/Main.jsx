45ab577c562c16ab2ba87d0f79d0ab487e08f30d
junxtar <aa1870088@naver.com>
2024-01-14 17:38:32 +0900

    Feat: Main Page Body 구성

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -2,11 +2,13 @@ import Footer from "../component/Footer.jsx";
 import Container from "@mui/material/Container";
 import * as React from "react";
 import Header from "../component/Header.jsx";
+import Body from "../component/Body.jsx";
 
 const Main = () => {
     return (
-        <Container component="main" maxWidth="md">
+        <Container component="main" maxWidth="sm">
             <Header/>
+            <Body/>
             <Footer/>
         </Container>
     )
