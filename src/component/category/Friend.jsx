8c995781df3b878a334efc847fc2b0bbb2cfb860
junxtar <aa1870088@naver.com>
2024-01-16 21:31:11 +0900

    Feat: 기본 응답 공통 에러처리 추가

--- src/component/category/Friend.jsx
+++ src/component/category/Friend.jsx
@@ -1,15 +1,25 @@
-import {List, Typography} from "@mui/material";
+import {Divider, List, Typography} from "@mui/material";
+import {getFriendList} from "../../api/friend/friendApi.js";
+
 
 export const Friend = () => {
+    getFriendList().then((response) => {
+        const list = response.data.data;
+        console.log(list);
+    });
+
     return (
         <List component="nav" aria-label="mailbox folders" sx={{ml: 2}}>
             <Typography variant="h6"
                         sx={{
-                            fontWeight: 'bold'
+                            fontWeight: 'bold',
+                            mb: 1
                         }}>
                 Friend
             </Typography>
+            <Divider/>
 
         </List>
     )
 }
+
