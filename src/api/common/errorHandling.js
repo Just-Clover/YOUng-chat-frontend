9c4858069fd18e2433c74928585994c539678ddc
junxtar <aa1870088@naver.com>
2024-01-17 23:27:37 +0900

    Refactor: 403 error -> login page 이동

--- src/api/common/errorHandling.js
+++ src/api/common/errorHandling.js
@@ -5,8 +5,10 @@ import errors from "./code.js";
  * @param {Error} error
  */
 export async function handleError(error) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     if (error.response.status === 403) {
         console.log(403);
+        window.location.href = "/login";
         return;
     }
     const code = error.response.data.code;
