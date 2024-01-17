dd9ad29d909cf74723fe6284b1686e18fa635bc3
junxtar <aa1870088@naver.com>
2024-01-17 22:21:28 +0900

    Refactor: errorHandling 로직 수정

--- src/api/common/errorHandling.js
+++ src/api/common/errorHandling.js
@@ -1,5 +1,4 @@
 import errors from "./code.js";
-import {deleteToken} from "./cookie.js";
 
 /**
  * 오류 공통처리
@@ -7,7 +6,6 @@ import {deleteToken} from "./cookie.js";
  */
 export async function handleError(error) {
     if (error.response.status === 403) {
-        deleteToken();
         console.log(403);
         return;
     }
