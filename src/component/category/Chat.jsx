52ef4c2134c51d97ae8640f21dc15f16471bcfae
junxtar <aa1870088@naver.com>
2024-01-17 15:08:59 +0900

    Feat: 채팅 목록 조회 추가

--- src/component/category/Chat.jsx
+++ src/component/category/Chat.jsx
@@ -1,15 +1,70 @@
-import {List, Typography} from "@mui/material";
-
+import {List, ListItem, ListItemText, Typography} from "@mui/material";
+import React, {useEffect, useState} from "react";
+import {getChatRoomList} from "../../api/chat-room/chatRoomApi.js";
+import chatRoomStore from "../../store/chat-room/ChatRoomStore.js";
+import ListItemButton from "@mui/material/ListItemButton";
+import Divider from "@mui/material/Divider";
 
 export const Chat = () => {
+    const {chatRoom, setChatRoom} = chatRoomStore();
+    const [isLoaded, setIsLoaded] = useState(false);
+
+    useEffect(() => {
+        if (!isLoaded) {
+            getChatRoomList().then((response) => {
+                setChatRoom(response.data.data);
+                setIsLoaded(true);
+            });
+        }
+    }, []);
+
+    const chatRoomClick = (room) => {
+        console.log(room);
+    };
     return (
         <List component="nav" aria-label="mailbox folders" sx={{ml: 2}}>
             <Typography variant="h6"
                         sx={{
-                            fontWeight: 'bold'
+                            fontWeight: 'bold',
+                            mb: 2
+
                         }}>
                 Chat
             </Typography>
+            <Divider/>
+            {chatRoom.map((room, index) => (
+                <ListItem
+                    disablePadding
+                    key={index}
+                    divider
+                    onClick={() => chatRoomClick(room)}>
+                    <ListItemButton>
+                        <ListItemText
+                            primary={
+                                <Typography
+                                    sx={{display: 'inline'}}
+                                    component="span"
+                                    variant="h6"
+                                    fontWeight="bold"
+                                >
+                                    {room.title}<br/>
+                                </Typography>
+                            }
+
+                            secondary={
+                                <Typography
+                                    component="span"
+                                    variant="body2"
+                                    color="gray"
+                                >
+                                    {room.lastChat}
+                                </Typography>
+                            }
+                        />
+                    </ListItemButton>
+                    <Divider/>
+                </ListItem>
+            ))}
         </List>
     )
 }
