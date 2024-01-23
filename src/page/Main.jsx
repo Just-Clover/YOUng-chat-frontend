c69e6390a25c90b6376dbae08525938959585b5b
junxtar <aa1870088@naver.com>
2024-01-23 23:59:54 +0900

    Refactor: Category 기본값 설정

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -13,7 +13,6 @@ const Main = () => {
     const navigate = useNavigate();
 
     useEffect(() => {
-
         const token = getCookie("RefreshToken");
         if (!token) {
             deleteToken();
--- src/store/category/CategoryStore.js
+++ src/store/category/CategoryStore.js
@@ -2,7 +2,7 @@ import {create} from 'zustand'
 import {persist} from "zustand/middleware";
 
 const categoryStore = create(persist(set => ({
-    category: "",
+    category: "friend",
     setCategory: (category) => set({category: category})
 }), {
     name: "category-store"
