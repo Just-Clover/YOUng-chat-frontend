e93641d4e20b8354448f8662044b31931164a223
junxtar <aa1870088@naver.com>
2024-01-25 14:50:01 +0900

    Refactor: 기본값 friend로 구성

--- src/store/main/MainBodyStore.js
+++ src/store/main/MainBodyStore.js
@@ -2,7 +2,7 @@ import {create} from "zustand";
 import {persist} from "zustand/middleware";
 
 const MainBodyStore = create(persist(set => ({
-    mainBody: "null",
+    mainBody: "friend",
     setMainBody: (mainBody) => set({mainBody: mainBody}),
 }), {
     name: "main-body-store"
