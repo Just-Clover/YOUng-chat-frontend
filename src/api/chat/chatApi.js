f19442cae24810614e6a0398e7fd61cd29b1a997
junxtar <aa1870088@naver.com>
2024-01-25 14:49:07 +0900

    Refactor: 채팅 보내기 API 삭제

--- src/api/chat/chatApi.js
+++ src/api/chat/chatApi.js
@@ -1,9 +1,5 @@
 import {chat} from "../index.js";
 
-export const inputChat = (chatRoomId, userData) => {
-    return chat.post(`/${chatRoomId}/chats`, userData);
-}
-
 export const deleteChat = (chatRoomId, chatId) => {
     return chat.delete(`/${chatRoomId}/chats/${chatId}`);
 }
