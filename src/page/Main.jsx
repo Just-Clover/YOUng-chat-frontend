7ffbc844ce5cbe3826215965cdddd044f506b325
junxtar <aa1870088@naver.com>
2024-01-29 16:49:40 +0900

    Refactor: Webstorm 연결 작업 Main 페이지로 이동

--- src/page/Main.jsx
+++ src/page/Main.jsx
@@ -7,18 +7,78 @@ import Sidebar from "../component/Sidebar.jsx";
 import {useEffect, useState} from "react";
 import {deleteToken, getCookie} from "../api/common/cookie.js";
 import {useNavigate} from "react-router-dom";
+import * as StompJs from "@stomp/stompjs";
+import {getChatRoomList, getDetailChatRoom} from "../api/chat-room/chatRoomApi.js";
+import chatRoomStore from "../store/chat-room/ChatRoomStore.js";
+import stompStore from "../store/stomp/StompStore.js";
+import selectedChatRoomStore from "../store/chat-room/SelectedChatRoomStore.js";
+import chatStore from "../store/chat/ChatStore.js";
+import userStore from "../store/user/UserStore.js";
 
 const Main = () => {
     const [open, setOpen] = useState(true);
     const navigate = useNavigate();
+    const {setChatRoom} = chatRoomStore();
+    const {setStompClient} = stompStore();
+    const {selectedChatRoomId} = selectedChatRoomStore();
+    const {setMessages} = chatStore();
+    const {userId} = userStore();
+
+    const fetchInitialMessages = async () => {
+        if (!selectedChatRoomId) return;
+        const response = await getDetailChatRoom(selectedChatRoomId);
+        const formattedMessages = formatMessages(response.data.data.chatResList);
+        setMessages(formattedMessages);
+    };
+
+    const formatMessages = (chatResList) => {
+        return chatResList.map((message, index, array) => ({
+            ...message,
+            showAvatarAndName: index === 0 || array[index - 1].userId !== message.userId
+        }));
+    };
 
     useEffect(() => {
         const token = getCookie("AccessToken");
         if (!token) {
             deleteToken();
-            navigate("/login", {replace : true});
+            navigate("/login", {replace: true});
+            return;
         }
-    }, []);
+        const client = new StompJs.Client({
+            brokerURL: import.meta.env.VITE_SOCKET_ROOT,
+            reconnectDelay: 500,
+            onConnect: () => {
+                console.log("WebSocket connected successfully");
+
+                if (selectedChatRoomId) {
+                    client.subscribe(`/exchange/chat.exchange/chat-rooms.` + selectedChatRoomId, () => {
+                        fetchInitialMessages().then(() => {
+                        });
+                    });
+                }
+
+                client.subscribe(`/exchange/chat.exchange/users.` + userId, () => {
+                    getChatRoomList().then(response => {
+                        console.log("test");
+                        setChatRoom(response.data.data);
+                    });
+                });
+            },
+            connectHeaders: {
+                AccessToken: getCookie("AccessToken"),
+            },
+            onStompError: (frame) => {
+                console.error("WebSocket error:", frame);
+            },
+            onDisconnect: () => {
+                console.log("WebSocket disconnected");
+            },
+        });
+        client.activate();
+        setStompClient(client);
+
+    }, [selectedChatRoomId]);
 
     return (
         <Box sx={{display: 'flex', flexDirection: 'column', minHeight: '98vh'}}>
